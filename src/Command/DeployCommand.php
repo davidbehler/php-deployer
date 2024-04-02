a1b9ba6a9bf5e2dbb812a8afc43d7cd75b4ee4ca
David Behler <d.behler@gmail.com>
2024-04-02 13:28:44 +0200

    wip

--- src/Command/DeployCommand.php
+++ src/Command/DeployCommand.php
@@ -4,6 +4,7 @@ namespace PhpDeployer\Command;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Filesystem\Filesystem;
 
 class DeployCommand extends BaseCommand
 {
@@ -15,24 +16,42 @@ class DeployCommand extends BaseCommand
 
         $now = new \DateTime('now', new \DateTimeZone('UTC'));
 
-        $releasePath = RELEASES_PATH.'release-'.$now->format('Y-m-d').'-'.$now->getTimestamp().'/';
+        $targetReleasePath = RELEASES_PATH.'release-'.$now->format('Y-m-d').'-'.$now->getTimestamp().'/';
+
+        $filesystem = new Filesystem;
+
+        $releasesConfigPath = PROJECT_PATH.'releases.json';
+
+        if($filesystem->exists($releasesConfigPath)) {
+            $releasesConfig = json_decode(file_get_contents($releasesConfigPath), true);
+        } else {
+            $releasesConfig = [];
+        }
+
+        dump($releasesConfig);
+        exit();
 
         $commandsToRun = [
             'deploy:ensure-directory-structure' => [
                 'options' => [
-                    'releasePath' => $releasePath
+                    'releasePath' => $targetReleasePath
                 ]
             ],
             'deploy:clone-repository' => [
                 'options' => [
-                    'releasePath' => $releasePath
+                    'releasePath' => $targetReleasePath
                 ]
             ],
             'deploy:compare-version' => [
                 'options' => [
-                    'releasePath' => $releasePath
+                    'releasePath' => $targetReleasePath
                 ],
                 'nonFailureStopCode' => 2
+            ],
+            'deploy:update-current-release-link' => [
+                'options' => [
+                    'releasePath' => $targetReleasePath
+                ],
             ]
         ];
 
